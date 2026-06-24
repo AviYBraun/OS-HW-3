f857d417f7791609dd5a2ccddd888234757bac0f
Avi Braun <avib1027@gmail.com>
2026-06-24 22:54:38 +0300

    implemented log struct, init and destroy functions

--- log.c
+++ log.c
@@ -5,17 +5,50 @@
 // Opaque struct definition
 struct Server_Log {
     // TODO: Implement internal log storage (e.g., dynamic buffer, linked list, etc.)
+    char* log_data;
+    int current_len;
+    int max_capacity;
+
+    int active_readers;
+    int active_writers;
+    int waiting_writers;
+
+    pthread_mutex_t lock;
+    pthread_cond_t read_allowed;
+    pthread_t write_allowed;
+
 };
 
 // Creates a new server log instance (stub)
 server_log create_log() {
     // TODO: Allocate and initialize internal log structure
-    return (server_log)malloc(sizeof(struct Server_Log));
+    server_log l = (server_log)malloc(sizeof(struct Server_Log));
+    if(!l){return NULL;}
+
+    l->max_capacity = 4096;
+    l->current_len = 0;
+    l->log_data = (char*)malloc(l->max_capacity);
+    if(l->log_data){
+        l->log_data[0] = '\0';
+    }
+
+    l->active_readers = 0;
+    l->active_writers = 0;
+    l->waiting_writers = 0;
+
+    return l;
+
 }
 
 // Destroys and frees the log (stub)
 void destroy_log(server_log log) {
     // TODO: Free all internal resources used by the log
+    if(!log){return;}
+
+    free(log_data);
+    pthread_mutex_destroy(&log->lock);
+    pthread_mutex_destroy(&log->read_allowed);
+    pthread_mutex_destroy(&log->write_allowed);
     free(log);
 }
 
